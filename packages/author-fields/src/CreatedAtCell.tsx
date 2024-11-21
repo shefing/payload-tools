79f7658861eb2738a48be4f0de67596ab9865c0e
rikifrank <rikif@shefing.com>
2024-11-21 12:01:05 +0200

    migrate to 3

--- packages/author-fields/src/CreatedAtCell.tsx
+++ packages/author-fields/src/CreatedAtCell.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { useTableCell } from '@payloadcms/ui';
 import React, { Fragment } from 'react';
 import moment from 'moment';
 
-export const CreatedAtCell: React.FC = () => {
-  const { cellData } = useTableCell();
+interface TableCellProps {
+  cellData: any;
+}
+export const CreatedAtCell: React.FC<TableCellProps> = (props) => {
+  const { cellData } = props;
   // Ensure cellData is not null before passing it to moment
   const validCellData = cellData !== null ? cellData : undefined;
 
