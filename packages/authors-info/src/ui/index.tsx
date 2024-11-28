10536effd915c723cc21d6f508e9dd8113986870
rikifrank <rikif@shefing.com>
2024-11-28 10:54:33 +0200

    CreatedAtCell

--- packages/authors-info/src/ui/index.tsx
+++ packages/authors-info/src/ui/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import { TableCellProps, CreatedAtCellClient } from './index.client.js';
 
-export const CreatedAtCell: React.FC<TableCellProps> = () => {
-  return <CreatedAtCellClient cellData={undefined} />;
+export const CreatedAtCell: React.FC<TableCellProps> = (props) => {
+  return <CreatedAtCellClient cellData={props} />;
 };
