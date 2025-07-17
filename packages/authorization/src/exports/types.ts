e8d6299f7ae459f068c5e83dcfcb063dfacbb93e
tsemachhadad <tsemachh@gmail.com>
2025-07-17 14:39:21 +0300

    Fix import in Docker build

--- packages/authorization/src/exports/types.ts
+++ packages/authorization/src/exports/types.ts
@@ -1,1 +1,8 @@
+import {isAdmin} from "../access/isAdmin";
+import {isAdminOrSelf} from "../access/isAdminOrSelf";
+import {isLoggedIn} from "../access/isLoggedIn";
+
 export type {AuthorizationPluginConfig} from '../types.js'
+export {isAdmin}
+export {isAdminOrSelf}
+export {isLoggedIn}
