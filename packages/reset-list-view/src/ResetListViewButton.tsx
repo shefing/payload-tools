6453a1824b4bd465e2cb7621c610c082cbff9832
tsemachhadad <tsemachh@gmail.com>
2025-08-18 12:04:19 +0300

    Dashboard with default filter

--- packages/reset-list-view/src/ResetListViewButton.tsx
+++ packages/reset-list-view/src/ResetListViewButton.tsx
@@ -2,8 +2,7 @@
 import { getTranslation } from '@payloadcms/translations'
 import {PopupList, useConfig, useTranslation } from '@payloadcms/ui'
 import { useAuth } from '@payloadcms/ui/providers/Auth'
-//import { stringify } from 'qs-esm'
-import {usePreferences} from "@payloadcms/ui";
+import { stringify } from 'qs-esm'
 
 type Props = {
   slug: string
@@ -14,7 +13,6 @@ export const ResetListViewButton: React.FC<Props> = ({ slug }) => {
   const { user } = useAuth()
   const {  i18n ,t} = useTranslation()
   const currentCollectionConfig = getEntityConfig({ collectionSlug:slug })
-  const { setPreference } = usePreferences()
 
   const {
     routes: { api },
@@ -22,31 +20,27 @@ export const ResetListViewButton: React.FC<Props> = ({ slug }) => {
   } = config
   const handleReset = async () => {
     try {
-      await setPreference(`collection-${slug}`,null)
-      window.location.reload()
-      /*
-            // Build the apiquery parameter for the user ID
-            const apiquery = {
-              depth: 0, // Fetch more details for a single company
-              where: {
-                key: { equals: `collection-${slug}` },
-                'user.id': { equals: user?.id },
-              },
-            }
-            const stringifyQuery = stringify(apiquery, { addQueryPrefix: true })
-            // Direct call to the Payload API to delete preferences for a specific view
-            const response = await fetch(`${serverURL}${api}/payload-preferences?${stringifyQuery}`, {
-              credentials: 'include',
-              method: 'DELETE',
-              mode: 'cors',
-            })
+      // Build the apiquery parameter for the user ID
+      const apiquery = {
+        depth: 0, // Fetch more details for a single company
+        where: {
+          key: { equals: `collection-${slug}` },
+          'user.id': { equals: user?.id },
+        },
+      }
+      const stringifyQuery = stringify(apiquery, { addQueryPrefix: true })
+      // Direct call to the Payload API to delete preferences for a specific view
+      const response = await fetch(`${serverURL}${api}/payload-preferences?${stringifyQuery}`, {
+        credentials: 'include',
+        method: 'DELETE',
+        mode: 'cors',
+      })
 
-            if (response.ok) {
-              window.location.reload()
-            } else {
-              console.error('Failed to reset list view preferences')
-            }
-      */
+      if (response.ok) {
+        window.location.reload()
+      } else {
+        console.error('Failed to reset list view preferences')
+      }
     } catch (error) {
       console.error('Error resetting list view preferences:', error)
     }
